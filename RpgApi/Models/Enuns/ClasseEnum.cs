c0a5fcdb29640ea0e24146bb5bad611d33b0d6d7
PaolaCondori <paola.condori@etec.sp.gov.br>
2025-02-17 07:46:51 -0300

    feat: Adicionando o projeto

---
+++ RpgApi/Models/Enuns/ClasseEnum.cs
@@ -0,0 +1,10 @@
+namespace RpgApi.Models.Enuns
+{
+    public enum ClasseEnum
+    {
+        //Se o valor não for especificado ele assume ovalor sequencial
+        Cavaleiro = 1,
+        Mago = 2,
+        Clerigo = 3
+    }
+}
